f61749486ebdb2abd1fb994eb315b74e3fa198fd
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-06-19 12:48:35 +0530

    ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,17 @@
-// Navbar.js
 import React from "react";
 import { useCart } from "../context/CartContext";
 
 const Navbar = () => {
-  const {
-    state: { cartItems },
-  } = useCart();
+  const { state: { cartItems } } = useCart();
 
-  const totalCount = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+  // Calculate total quantity (sum of all items quantities)
+  const totalQuantity = cartItems.reduce((acc, item) => acc + item.quantity, 0);
 
   return (
     <nav>
-      <h1>useReducer</h1>
-      <div>
-        <span id="nav-cart-item-count">Cart ({totalCount})</span>
-      </div>
+      <h1>My Cart App</h1>
+      <div id="nav-cart-item-count">{totalQuantity}</div>
+      <p>useReducer</p>
     </nav>
   );
 };
