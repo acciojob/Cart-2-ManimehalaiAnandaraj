ccba74313b49b12ae57382b325e2131c1cc2c208
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-06-19 12:10:30 +0530

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,7 @@ function AppWithContext() {
 }, []);
 
 
+
   return (
     <div id="main">
       <Navbar />
--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -3,21 +3,22 @@
 const cartReducer = (state, action) => {
   switch (action.type) {
     case "ADD_ITEM":
-      const existing = state.cartItems.find(item => item.id === action.payload.id);
-      if (existing) {
-        return {
-          ...state,
-          cartItems: state.cartItems.map(item =>
-            item.id === action.payload.id
-              ? { ...item, quantity: item.quantity + 1 }
-              : item
-          ),
-        };
-      }
-      return {
-        ...state,
-        cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }],
-      };
+  const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+  if (existingItem) {
+    return {
+      ...state,
+      cartItems: state.cartItems.map(item =>
+        item.id === action.payload.id
+          ? { ...item, quantity: item.quantity + 1 }
+          : item
+      )
+    };
+  }
+  return {
+    ...state,
+    cartItems: [...state.cartItems, { ...action.payload, quantity: 1 }]
+  };
+
 
     case "INCREMENT":
       return {
